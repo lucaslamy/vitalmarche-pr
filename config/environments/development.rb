29c3d8fb0ea5fcc22534cf2a65417ede8e82d152
Unknown <lucaslamy87@yahoo.fr>
2019-12-25 21:44:42 +0100

    Edit user view

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  config.action_mailer.asset_host
 
   # Show full error reports.
   config.consider_all_requests_local = true
