7d3c6035f3007afe58073755c9507509feca8013
Unknown <lucaslamy87@yahoo.fr>
2019-12-25 22:40:43 +0100

    Update mailer view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   namespace :admin do
@@ -5,12 +6,11 @@ Rails.application.routes.draw do
       resources :events
       resources :articles
       resources :albums
-
       root to: "users#index"
       post '/users/send_invitation(/:name)(/:email)', to: "users#send_invitation", as: :invitation
   end
+
   devise_for :users, controllers: {registrations: 'users/registrations'}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "main#home", as: :root
   get '/adherents', to: "user_space#main", as: :user_space
   get '/adherents/user', to: "user_space#simple_user_space", as: :simple_user_space
